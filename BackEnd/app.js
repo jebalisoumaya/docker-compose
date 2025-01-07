941c351d514bc06c332fda7647616dc9bcac5d36
jebalisoumaya <soumaya.jebali99@gmail.com>
2025-01-07 13:06:51 +0100

    final 0.2

---
+++ BackEnd/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = 8060;
+
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+
+mongoose
+  // .connect("mongodb://127.0.0.1:5000/fanTrade", {})
+  .connect("mongodb://mongo:27017/fanTrade", {})
+  .then(() => {
+    console.log("Connected to the mongoDB database!");
+  })
+  .catch((err) => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
+const routes = require("./routes/routes");
+app.use("/", routes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
